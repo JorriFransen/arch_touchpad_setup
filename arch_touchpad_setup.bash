4d31a5fb93f83e76a79a8f609fb1423cbc9fad3f
Jorri Fransen <jorrifransen@gmail.com>
2017-10-03 22:25:15 +0200

    Eror checking.

--- arch_touchpad_setup.bash
+++ arch_touchpad_setup.bash
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
 TOUCHPAD_ID=$(xinput | grep TouchPad | grep -P -o '(?<=\sid=)\d+')
+
+if [[ $TOUCHPAD_ID == ^-?[0-9]+$ ]] 
+	then
+	echo Could not find touchpad id
+fi
+
 echo Found touchpad with id: $TOUCHPAD_ID
 
 TAP_ID=$(xinput list-props $TOUCHPAD_ID | grep -P -o '(?<=Tapping Enabled \()\d+')
-echo Found tap property id: $TAP_ID
+
+if [[ $TAP_ID == ^-?[0-9]+$ ]] 
+	then
+	echo Could not tap property for touchpad $TOUCHPAD_ID
+fi
+
+echo Found tap property for touchpad $TOUCHPAD_ID: $TAP_ID
 
 xinput set-prop $TOUCHPAD_ID $TAP_ID 1
 echo Tapping enabled
